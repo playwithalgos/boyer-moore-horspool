980aee56ea2e8ffd7cadedcc9d254793cc3b4d36
Francois Schwarzentruber <fschwarz@schumann.irisa.fr>
2023-05-14 16:16:17 +0200

    win-loose

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
-const CHARWIDTH = 40;
-let pos = 0;
+
 
 function randomWord(n) { return Array.from({ length: n }, () => Math.floor(Math.random() * 4)); }
 
 const alphabets = [0, 1, 2, 3];
-const word = randomWord(50);
+const word = randomWord(40);
 const pattern = randomWord(5);
 const colors = ["#EE4444", "#448822", "#4477DD", "#EEAA00"];
+
+let pos = 0;
+let cursor = pattern.length - 1;
+
+const lastOcc = {};
+
+for (let i = 0; i < pattern.length - 1; i++) {
+    lastOcc[pattern[i]] = i;
+}
 
 function fill(el, word) {
     for (const char of word) {
@@ -21,10 +29,18 @@ function fill(el, word) {
 fill(document.getElementById("word"), word);
 fill(document.getElementById("pattern"), pattern);
 
+const CHARWIDTH = (document.getElementById("word").children[word.length - 1].getBoundingClientRect().left - document.getElementById("word").children[0].getBoundingClientRect().left) / word.length;
+function nextCorrectPos() {
+    return pos + (lastOcc[word[pos + pattern.length - 1]] != undefined ? (pattern.length - 1 - lastOcc[word[pos + pattern.length - 1]]) : pattern.length);
+}
+
+
 function moveHorizontallyOnDrag(el) {
     let x = 0;
     el.onmousedown = (evt) => {
         reset();
+        document.getElementById("word").children[cursor].classList.add("cursor");
+
         x = evt.pageX;
 
         document.onmousemove = (evt) => {
@@ -35,10 +51,30 @@ function moveHorizontallyOnDrag(el) {
         }
 
         document.onmouseup = (evt) => {
-            pos = Math.round(parseInt(el.style.left ? el.style.left : 0) / CHARWIDTH);
-            if(pos < 0) pos = 0;
-            if(pos > word.length - pattern.length-1) pos= word.length - pattern.length - 1;
-            el.style.left = (pos * CHARWIDTH) + "px";
+
+            let bestX = 100000;
+            let newpos = undefined
+            for (let i = 0; i < word.length; i++) {
+                const d = Math.abs(document.getElementById("word").children[i].getBoundingClientRect().left - document.getElementById("pattern").getBoundingClientRect().left);
+                if (d < bestX) {
+                    bestX = d;
+                    newpos = i;
+                }
+            }
+
+            if (newpos == nextCorrectPos()) {
+                pos = newpos;
+                document.getElementById("pattern").classList.add("bravo");
+                setTimeout(() => document.getElementById("pattern").classList.remove("bravo"), 500);
+            }
+            else {
+                console.log("correction position :", nextCorrectPos());
+                console.log("wanted position", newpos);
+                document.body.classList.add("error");
+                setTimeout(() => document.body.classList.remove("error"), 500);
+            }
+
+            el.style.left = document.getElementById("word").children[pos].getBoundingClientRect().left - document.getElementById("word").getBoundingClientRect().left + "px";
             update();
         }
     }
@@ -55,17 +91,18 @@ function reset() {
         e.classList.remove("dismatch");
         e.classList.remove("cursor");
     });
-    document.getElementById("word").children[pos + pattern.length - 1].classList.add("cursor");
 
 }
+
+
 
 
 function update() {
     reset();
 
     let k = pattern.length - 1;
-
-    if(!document.getElementById("horspool").checked) {
+    cursor = pos + k;
+    if (!document.getElementById("horspool").checked) {
         for (k = pattern.length - 1; k >= 0; k--) {
             if (pattern[k] == word[pos + k]) {
                 document.getElementById("pattern").children[k].classList.add("match");
@@ -74,20 +111,16 @@ function update() {
             else {
                 document.getElementById("pattern").children[k].classList.add("dismatch");
                 document.getElementById("word").children[pos + k].classList.add("dismatch");
+                cursor = pos + k;
                 break;
             }
         }
     }
+    document.getElementById("word").children[cursor].classList.add("cursor");
 
-    const lastOcc = {};
-
-    for (let i = 0; i < k; i++) {
-        lastOcc[pattern[i]] = i;
-    }
-
-    for(let letter in lastOcc) 
+    for (let letter in lastOcc)
         document.getElementById("pattern").children[lastOcc[letter]].classList.add("lastOccurrence");
-    
+
 
 }
 
